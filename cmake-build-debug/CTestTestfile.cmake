2c22316b57b3ec131a5bcde9b153e7e8155bab5d
kothavandlanaresh <kothavandlanaresh@gmail.com>
2024-09-05 02:08:01 +0200

    initial commit

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/nareshkothavandla/Documents/CLion
+# Build directory: /Users/nareshkothavandla/Documents/CLion/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[MyTestSuite]=] "/Users/nareshkothavandla/Documents/CLion/cmake-build-debug/MyTests")
+set_tests_properties([=[MyTestSuite]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/nareshkothavandla/Documents/CLion/CMakeLists.txt;28;add_test;/Users/nareshkothavandla/Documents/CLion/CMakeLists.txt;0;")
